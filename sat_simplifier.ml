78fbbf094ad3797d2927fc02ea63c4e35bcd4038
gaubian <GuilAub10@hotmail.fr>
2015-08-21 21:57:06 +0200

    Added some comments on sat-simplifier

--- sat_simplifier.ml
+++ sat_simplifier.ml
@@ -1,5 +1,5 @@
 (* f (A /\ (B \/ C)) -> f(A /\ B) \/ f(A /\ C) *)
-
+(* Here's the representation of a «generalized» formula *)
 type formula =  | X of int
 		| Not of formula
 		| Or of formula * formula
@@ -10,6 +10,7 @@ type formula =  | X of int
 		| Nor of formula * formula
 		| Nand of formula * formula
 
+(* Transform a generalized formula into a And-Or-Not formula *)
 let rec simpl = function
 	| Not(a)	-> Not(simpl a)
 	| Or(a,b)	-> Or(simpl a, simpl b)
@@ -21,6 +22,7 @@ let rec simpl = function
 	| Nand(a,b)	-> simpl @@ Not(And(a,b))
 	| x		-> x
 
+(* Descends «Not»s *)
 let rec erase = function
 	| Not(Not(x))	-> simpl x
 	| Not(Or(a,b))	-> And(erase @@ Not(a), erase @@ Not(b))
@@ -29,6 +31,7 @@ let rec erase = function
 	| And(a,b)	-> And(erase a, erase b)
 	| x		-> x
 
+(* Puts it into DNF *)
 let rec satSolve = function
 	| Or(a,b)		-> Or(satSolve a, satSolve b)
 	| And(a,Or(b,c))	-> Or( @@ And(a,b),f @@ And(b,c))
